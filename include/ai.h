c38b28a7b55f7619c7b153248e888fd8c4104a24
Gianluca Delgado <gdelgado@caltech.edu>
2019-05-29 18:29:01 -0700

    Implemented first version of ai

---
+++ include/ai.h
@@ -0,0 +1,9 @@
+#ifndef __AI_H__
+#define __AI_H__
+
+#include "scene.h"
+#include "body.h"
+
+void set_paddle_vel(Body *paddle, Body *ball);
+
+#endif // #ifndef __AI_H__
---
+++ library/ai.c
@@ -0,0 +1,25 @@
+#include "ai.h"
+#include "pong.h"
+
+void set_paddle_vel(Body *paddle, Body *ball){
+  Vector ball_vel = body_get_velocity(ball);
+  Vector paddle_vel = body_get_velocity(paddle);
+  Vector ball_cent = body_get_centroid(ball);
+  Vector paddle_cent = body_get_centroid(paddle);
+
+  if(ball_vel.x < 0){
+    ball_vel.x *= -1;
+  }
+
+  double x_pos_diff = paddle_cent.x - ball_cent.x;
+  double t = x_pos_diff / ball_vel.x;
+
+  double final_y_pos = ball_vel.y *(t) + ball_cent.y;
+
+  double y_diff = final_y_pos - paddle_cent.y;
+
+  double new_y_vel = y_diff/t;
+
+  body_set_velocity(paddle, (Vector){0, new_y_vel});
+
+}
