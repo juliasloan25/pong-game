ecab08199b0e8c09dc45c6e11651413bb808102f
Gianluca Delgado <gdelgado@caltech.edu>
2019-05-29 18:45:48 -0700

    Made ai beatable

--- library/ai.c
+++ library/ai.c
@@ -8,18 +8,21 @@ void set_paddle_vel(Body *paddle, Body *ball){
   Vector paddle_cent = body_get_centroid(paddle);
 
   if(ball_vel.x < 0){
-    ball_vel.x *= -1;
+    body_set_velocity(paddle, VEC_ZERO);
+    return;
   }
 
   double x_pos_diff = paddle_cent.x - ball_cent.x;
-  double t = x_pos_diff / ball_vel.x;
+  if(x_pos_diff > 5){
+    double t = x_pos_diff / ball_vel.x;
 
-  double final_y_pos = ball_vel.y *(t) + ball_cent.y;
+    double final_y_pos = ball_vel.y *(t) + ball_cent.y;
 
-  double y_diff = final_y_pos - paddle_cent.y;
+    double y_diff = final_y_pos - paddle_cent.y;
 
-  double new_y_vel = y_diff/t;
+    double new_y_vel = y_diff/t;
 
-  body_set_velocity(paddle, (Vector){0, new_y_vel});
+    body_set_velocity(paddle, (Vector){0, new_y_vel/2});
+  }
 
 }
